522de20a1c2fa0ddf813e0a25ea24e1ec4103b24
Jefferson Thiago <jeffersonthiago.ga@gmail.com>
2023-06-30 19:23:17 -0300

    adicionando classes para o cliente HTTP que empacotará os métodos do cURL em uma interface mais simples

---
+++ include/HttpClient.hpp
@@ -0,0 +1,11 @@
+#if !defined(HTTP_CLIENT_HPP)
+#define HTTP_CLIENT_HPP
+
+#include <map>
+
+class HttpClient
+{
+public:
+};
+
+#endif // HTTP_CLIENT_HPP
---
+++ include/HttpMethod.hpp
@@ -0,0 +1,16 @@
+#if !defined(HTTP_METHOD_HPP)
+#define HTTP_METHOD_HPP
+
+enum class HttpMethod
+{
+  GET,
+  POST,
+  PUT,
+  PATCH,
+  DELETE,
+  CONNECT,
+  OPTIONS,
+  TRACE
+};
+
+#endif // HTTP_METHOD_HPP
