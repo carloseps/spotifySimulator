764c3a9eceb566d162e69c0f2cc44ecd77320256
Jefferson Thiago <jeffersonthiago.ga@gmail.com>
2023-07-02 19:04:46 -0300

    adicionando exemplo de POST e GET na classe main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <curl/curl.h>
 #include <iostream>
+#include <sstream>
 #include <nlohmann/json.hpp>
 #include <Url.hpp>
 #include <HttpStatus.hpp>
@@ -9,8 +10,8 @@
 int main(int argc, char const *argv[])
 {
     RestClient *restClient = new RestClient();
-    std::string url = "https://accounts.spotify.com/api/token";
-    ResponseEntity<nlohmann::json> *responseEntity;
+    std::string tokenUrl = "https://accounts.spotify.com/api/token";
+    ResponseEntity<nlohmann::json> *postResponseEntity;
 
     std::map<std::string, std::string> formData;
     formData["client_id"] = "d793a5bbf03749b1a5454ac339001842";
@@ -21,12 +22,29 @@ int main(int argc, char const *argv[])
     headers["Content-Type"] = "application/x-www-form-urlencoded";
 
     // TO DO: Especificar ContentType na chamada da função
-    responseEntity = restClient->post(url, formData, headers);
+    postResponseEntity = restClient->post(tokenUrl, formData, headers);
 
-    nlohmann::json data = responseEntity->getBody();
+    nlohmann::json token = postResponseEntity->getBody();
 
-    std::cout << data["access_token"] << std::endl;
-    std::cout << data["token_type"] << std::endl;
+    std::cout << token["access_token"].get<std::string>() << std::endl;
+    std::cout << token["token_type"].get<std::string>() << std::endl;
+
+    std::string getTrackUrl = "https://api.spotify.com/v1/tracks/11dFghVXANMlKmJXsNCbNl";
+
+    ResponseEntity<nlohmann::json> *getResponseEntity;
+
+    headers.erase("Content-Type");
+
+    std::ostringstream oStringStream;
+    oStringStream << token["token_type"].get<std::string>() << " " << token["access_token"].get<std::string>();
+    headers["Authorization"] = oStringStream.str();
+
+    std::cout << oStringStream.str() << std::endl;
+
+    getResponseEntity = restClient->get(getTrackUrl, headers);
+    nlohmann::json data = getResponseEntity->getBody();
+
+    std::cout << data << std::endl;
 
     return 0;
 }
